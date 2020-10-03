d49c120ad3d64b30441747e1d8bf9df0ba179307
João Costa <jdpc557@gmail.com>
2020-10-03 10:49:59 +0100

    Add basic menu and intro

--- shared/src/main/scala/eu/joaocosta/ld47/AppState.scala
+++ shared/src/main/scala/eu/joaocosta/ld47/AppState.scala
@@ -1,4 +1,9 @@
 package eu.joaocosta.ld47
 
-case class AppState(x: Double, y: Double, rotation: Double)
+sealed trait AppState
+object AppState {
+  case object Menu extends AppState
+  case class Intro(scale: Double) extends AppState
+  case class GameState(x: Double, y: Double, rotation: Double) extends AppState
+}
 
--- shared/src/main/scala/eu/joaocosta/ld47/Main.scala
+++ shared/src/main/scala/eu/joaocosta/ld47/Main.scala
@@ -11,11 +11,6 @@ import scala.concurrent.duration._
 
 object Main extends MinartApp {
 
-  val initialGameState = AppState(
-    x = 920.0,
-    y = 580.0,
-    rotation = 0.0)
-
   type State = AppState
   val renderLoop = RenderLoop.default()
   val canvasSettings = Canvas.Settings(
@@ -23,25 +18,29 @@ object Main extends MinartApp {
     height = 224,
     scale = 2)
   val canvasManager: CanvasManager = CanvasManager.default(canvasSettings)
-  val initialState: AppState = initialGameState
+  val initialState: AppState = AppState.Menu
   val frameRate = FrameRate.fps60
   val terminateWhen = (_: State) => false
+
+  val initialGameState = AppState.GameState(
+    x = 920.0,
+    y = 580.0,
+    rotation = 0.0)
 
   val resourceLoader = ResourceLoader.default()
 
   val background = Image.loadPpmImage(resourceLoader.loadResource("bg.ppm"))
+  val logo = Image.loadPpmImage(resourceLoader.loadResource("logo.ppm"))
   val map = Image.loadPpmImage(resourceLoader.loadResource("map.ppm"))
   val character = Image.loadPpmImage(resourceLoader.loadResource("char.ppm"))
 
   val tau = 2 * math.Pi
 
+  val renderLogo: CanvasIO[Unit] = logo.map(_.render(0, 0, Some(Color(0, 0, 0)))).getOrElse(CanvasIO.noop)
   val renderBackground: CanvasIO[Unit] = background.map(_.render(0, 0)).getOrElse(CanvasIO.noop)
   val renderChar: CanvasIO[Unit] = character.map(_.render(128 - 8, 112 - 8, Some(Color(255, 255, 255)))).getOrElse(CanvasIO.noop)
 
-  def renderMap(posX: Double, posY: Double, rot: Double) = {
-    val transform = Transformation.Translate(-posX, -posY)
-      .andThen(Transformation.Rotate(rot))
-      .andThen(Transformation.Translate(128, 112))
+  def renderMap(transform: Transformation) = {
     val pixels = for {
       x <- 0 until 256
       y <- 0 until 224
@@ -51,11 +50,15 @@ object Main extends MinartApp {
     CanvasIO.sequence_(pixels)
   }
 
-  def renderAppState(state: AppState): CanvasIO[Unit] = {
-    renderBackground.andThen(renderMap(state.x, state.y, state.rotation)).andThen(renderChar)
+  def renderGameState(state: AppState.GameState): CanvasIO[Unit] = {
+    val mapTransform =
+      Transformation.Translate(-state.x, -state.y)
+        .andThen(Transformation.Rotate(state.rotation))
+        .andThen(Transformation.Translate(128, 112))
+    renderBackground.andThen(renderMap(mapTransform)).andThen(renderChar)
   }
 
-  def updateAppState(state: AppState, keyboardInput: KeyboardInput): AppState = {
+  def updateGameState(state: AppState.GameState, keyboardInput: KeyboardInput): AppState = {
     val maxSpeed = map.toOption.flatMap(_.pixels.lift(state.y.toInt).flatMap(_.lift(state.x.toInt))).map(_.r / 255.0 * 5).getOrElse(5.0)
     val newRot =
       if (keyboardInput.isDown(Key.Left)) state.rotation - 0.05
@@ -73,7 +76,6 @@ object Main extends MinartApp {
       else newRot
     val nextX = state.x + speedX
     val nextY = state.y + speedY
-    println(map.toOption.flatMap(_.pixels.lift(nextY.toInt).flatMap(_.lift(nextX.toInt))))
     val stopped = map.toOption.flatMap(_.pixels.lift(nextY.toInt).flatMap(_.lift(nextX.toInt))).contains(Color(0, 0, 0))
     if (stopped)
       state.copy(rotation = normalizedRot)
@@ -96,13 +98,32 @@ object Main extends MinartApp {
   }
 
   val renderFrame = (state: State) => state match {
-    case gs @ AppState(x, y, rot) =>
+    case AppState.Menu =>
+      for {
+        keyboard <- CanvasIO.getKeyboardInput
+        _ <- CanvasIO.clear()
+        _ <- renderBackground.andThen(renderLogo)
+        newState = if (keyboard.isDown(Key.Enter)) AppState.Intro(0.005) else AppState.Menu
+        _ <- CanvasIO.redraw
+      } yield newState
+    case AppState.Intro(scale) =>
+      for {
+        _ <- CanvasIO.clear()
+        transform = Transformation.Translate(-initialGameState.x, -initialGameState.y)
+          .andThen(Transformation.Scale(scale))
+          .andThen(Transformation.Rotate(scale * tau))
+          .andThen(Transformation.Translate(128, 112))
+        _ <- renderBackground.andThen(renderMap(transform))
+        newState = if (scale >= 1.0) initialGameState else AppState.Intro(scale + 0.005)
+        _ <- CanvasIO.redraw
+      } yield newState
+    case gs @ AppState.GameState(x, y, rot) =>
       for {
         keyboard <- CanvasIO.getKeyboardInput
         _ = frameCounter()
         _ <- CanvasIO.clear()
-        _ <- renderAppState(gs)
-        newState = updateAppState(gs, keyboard)
+        _ <- renderGameState(gs)
+        newState = updateGameState(gs, keyboard)
         _ <- CanvasIO.redraw
       } yield newState
   }
